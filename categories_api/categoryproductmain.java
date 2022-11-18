991a04204977a3d45141f026a2dc575dd5735d34
rajanish-1631 <118096701+rajanish-1631@users.noreply.github.com>
2022-11-18 13:00:30 +0530

    Create categoryproductmain.java

---
+++ categories_api/categoryproductmain.java
@@ -0,0 +1,16 @@
+package Category_Details;
+
+import org.springframework.boot.*;
+
+@SpringBootApplication
+public class Category_Product_main {
+
+	public static void main(String[] args) {
+		
+SpringApplication.run(Category_Product_main.class, args);
+
+	}
+
+}
+
+
